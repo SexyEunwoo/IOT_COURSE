55409e64df1e4ef1db8ae80f4ddfcc5b1fda28f0
SexyEunwoo <dmsdndla1@gmail.com>
2021-01-26 17:24:00 +0900

    2021-01-26 Shell Script Study

--- Linux/Default Setting Shell Script/set.sh
+++ Linux/Default Setting Shell Script/set.sh
@@ -13,51 +13,63 @@ parameters=$@
 
 function set()
 {
-	local parameter=$1
-	if [ ${parameter} == "all" ]; then
-		for target in "${target_all[@]}"
-		do
-			echo "TARGET : ${target}"
-			set_configuration ${target}
-		done
-	fi	
+    local parameter=$1
+    if [ ${parameter} == "all" ]; then
+        for target in "${target_all[@]}"
+        do
+            echo "TARGET : ${target}"
+            set_configuration ${target}
+        done
+    fi
 }
 
 function set_configuration()
 {
-	target=$1
+    target=$1
 	# Check if there were files in current directory
-	if [ ! -f ${cur_path}/${target} ]; then
-		echo "There is no ${target} file"
-		echo "Downloading...."
-		wget https://raw.githubusercontent.com/SexyEunwoo/IOT_COURSE/main/Linux/Default%20Setting%20Files/${target}
-		if [ ! -f ${cur_path}/${target} ]; then
-			echo "Download Failed"
-			exit -1
-		fi
-	fi
+    if [ ! -f ${cur_path}/${target} ]; then
+        echo "There is no ${target} file"
+        echo "Downloading...."
+        wget https://raw.githubusercontent.com/SexyEunwoo/IOT_COURSE/main/Linux/Default%20Setting%20Files/${target}
+        if [ ! -f ${cur_path}/${target} ]; then
+            echo "Download Failed"
+            exit -1
+        fi
+    fi
 
-	# copy configure file to home dir
-	if [ ${target} == ${target_bash} ]; then
-		echo "Adding .bashrc content to ~/.bashrc"
-		cat ${cur_path}/${target} >> ${root_path}/${target}
-		echo "Adding Done!"
-	else
-		echo "Copying ${target} file to home directory..."
-		cp ${cur_path}/${target} ${root_path}
-		echo "Copying Done!"
-	fi
-	echo ""	
+    # copy configure file to home dir
+    if [ ${target} == ${target_bash} ]; then
+        echo "Adding .bashrc content to ~/.bashrc"
+        cat ${cur_path}/${target} >> ${root_path}/${target}
+        echo "Adding Done!"
+    fi
+
+    if [ ${target} == ${target_vi} ]; then
+        echo "Copying ${target} file to home directory..."
+        cp ${cur_path}/${target} ${root_path}
+        echo "Copying Done!"
+
+        echo "Vundle Plugin Download..."
+        git clone https://github.com/VundleVim/Vundle.vim.git ${root_path}/.vim/bundle/Vundle.vim
+        echo "Vundle Plugin Download Done!"
+		    fi
+
+    if [ ${target} == ${target_tmux} ]; then
+        echo "Copying ${target} file to home directory..."
+        cp ${cur_path}/${target} ${root_path}
+        echo "Copying Done!"
+    fi
+    #
 }
 
 # 1. Check Parameter Count( parameter is coming like "all" "vi" "tmux" etc.. )
 if [ ${parameter_cnt} -eq 0 ]; then
-	echo "Usage <Parameter1> <...>"
-	exit -1
+    echo "Usage <Parameter1> <...>"
+    exit -1
 fi
 
 # 2. send parameters to set function at a time
 for parameter in ${parameters[@]}
 do
-	set ${parameter}
-done
+    set ${parameter}
+done  
